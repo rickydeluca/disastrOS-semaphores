c40ac8f01a26d6159bc2e1cb3b0b895de2062abf
Riccardo De Luca <rickydeluca1@gmail.com>
2019-09-21 18:56:52 +0200

    Declared in disastrOS.h the variable semaphore_list defined in disastrOS.c

--- disastrOS.h
+++ disastrOS.h
@@ -13,6 +13,8 @@
 
 #endif //_DISASTROS_DEBUG_
 
+extern ListHead semaphores_list;
+
 // initializes the structures and spawns a fake init process
 void disastrOS_start(void (*f)(void*), void* args, char* logfile);
 
