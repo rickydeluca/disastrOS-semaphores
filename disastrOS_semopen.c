7bcdbd6b219fb9fcea23f527a6a1a7e2b8fb9970
Riccardo De Luca <rickydeluca1@gmail.com>
2019-09-21 17:59:08 +0200

    Insert allocation of fd e fd_ptr and insert them in the pcb

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -7,7 +7,7 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semOpen() {
-
+  
   // Get the id and the count value of the semaphore
   int sem_id = running->syscalls_args[0];
   int sem_count = running->syscalls_args[1];
@@ -18,14 +18,65 @@ void internal_semOpen() {
   Semaphore* sem = SemaphoreList_byId(&semaphores_list, sem_id);
 
   if (sem) {
-      running->syscall_retvalue = DSOS_ESEMAPHORECREATE;    // The error says that there was already an opened semaphore with the same ID
+      printf("There is already an opened semaphore with the ID: %d\n", sem_id);
+      running->syscall_retvalue = DSOS_ESEMAPHORECREATE;    
       return;
     }
 
   // Allocate new semaphore and add it to the global list
-  printf("Allocating semaphore with ID: %d\n", sem_id);
+  printf("Allocating new semaphore with ID: %d\n", sem_id);
+
   sem = Semaphore_alloc(sem_id, sem_count);
-  
+  if (!sem) {                                               // Check if the semaphore was allocated with no problems
+    printf("ERROR: Semaphore allocation!\n");
+    running->syscall_retvalue = DSOS_ECREATESEM;
+    return;
+  }
+
+  printf("Semaphore allocation completed\n")
+
   // Add created semaphore to global list
   List_insert(&semaphores_list, semaphores_list.last, (ListItem*)sem);
+
+  // Create the semaphore descritor
+  printf("Allocating SemDescriptor\n");
+
+  SemDescriptor* sem_fd = SemDescriptor_alloc(running->last_sem_fd, sem, running);
+  if (!desc) {
+     printf("ERROR: SemDescriptor allocation!\n");
+     running->syscall_retvalue = DSOS_ECREATEFD;
+     return;
+  }
+
+  printf("SemDescriptor allocation completed\n");
+
+  // Increment last_sem_fd number so the next semaphore can take a different file descriptor.
+  // It also represents the number of opened semaphores for this process
+  running->last_sem_fd++;
+
+  // Insert the new descriptor into the process descriptor list
+  List_insert(&running->sem_descriptors, running->sem_descriptors.last, (ListItem*) sem_fd);
+
+  printf("Allocating SemDescriptorPtr\n");
+
+  // Create the pointer to the file descriptor
+  SemDescriptorPtr* sem_fd_ptr = SemDescriptorPtr_alloc(sem_fd);
+  if(!sem_fd_ptr) {
+      printf("ERROR: SemDescriptorPtr allocation!\n");
+      running->syscall_retvalue = DSOS_ECREATEPTR;
+      return;
+  }
+
+  printf("SemDescriptorPtr allocation completed!\n");
+
+  // Add the descriptor to the semaphore created
+  printf("Adding despitor to the semaphore struct...");
+
+  sem_fd->ptr = sem_fd_ptr;
+  List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*) sem_fd_ptr);
+
+  printf("Done!\n");
+
+  // Return the sem descriptor to the process
+  running->syscall_retvalue = sem_fd->fd;
 }
