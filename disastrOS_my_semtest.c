9bc47c5f01ca069866e4a0907c1a91c70fc4e5c5
Riccardo De Luca <rickydeluca1@gmail.com>
2019-09-22 19:44:03 +0200

    Added a new test for semClose. Reformtted test layout introducing keyboard input.

--- disastrOS_my_semtest.c
+++ disastrOS_my_semtest.c
@@ -4,28 +4,72 @@
 #include "disastrOS.h"
 #include "disastrOS_semaphore.h"
 
-/* Testing semOpen */
+#define EXIT_FAILURE        1
+#define EXIT_SUCCESSFUL     0
+
+/* SEMOPEN TEST */
 void semopen_test(void* args) {
     int sem_id  = 1;
     int count   = 1;
     if (disastrOS_semOpen(sem_id, count) < 0) {
         printf("TEST ERROR: semOpen\n");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     printf("TEST SUCCESSFUL: semOpen\n");
+    exit(EXIT_SUCCESSFUL);
+
+}
+
+/* SEMCLOSE TEST */
+void semclose_test(void* args) {
+    
+    int sem_id  = 1;
+    int count   = 1;
+
+    printf("Opening semaphore with ID: %d...\n", sem_id);
+
+    if (disastrOS_semOpen(sem_id, count) < 0) {
+        printf("TEST ERROR: semOpen\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //printf(" Done!\n");
+
+    printf ("Closing the semaphore with ID: %d...\n", sem_id);
+
+    if (disastrOS_semClose(sem_id) != 0) {
+        printf("TEST ERROR: semClose\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //printf(" Done!\n");
+
+    printf("TEST SUCCESSFUL: semClose\n");
+    exit(EXIT_SUCCESSFUL);
 }
 
 int main(int argc, char** argv) {
+    int test_num;
     
     char* logfilename=0;
     if (argc>1) {
         logfilename = argv[1];
     }
    
-    // TEST: semoOpen
-    printf("Testing semOpen\n");
-    disastrOS_start(semopen_test, 0, logfilename);
+    // TEST: semOpen
+    printf("What would you like to test?\n    1 - semOpen\n    2 - semClose\n");
+
+    scanf("%d", &test_num);
+    if (test_num == 1) {
+        printf("Testing semOpen\n");
+        disastrOS_start(semopen_test, 0, logfilename);
+    } else if (test_num == 2) {
+        printf("Testing semClose\n");
+        disastrOS_start(semclose_test, 0, logfilename);
+    } else {
+        printf("Wrong input. Sayonara!");
+    }
 
     return 0;
 }
