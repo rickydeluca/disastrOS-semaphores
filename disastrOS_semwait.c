c1c8880315e855fb43e4da98687ec2d3a0f4d6ab
Riccardo De Luca <rickydeluca1@gmail.com>
2019-09-23 19:48:03 +0200

    Implemented semWait with decresing count and handling case when count <= 0

--- disastrOS_semwait.c
+++ disastrOS_semwait.c
@@ -6,6 +6,45 @@
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 
-void internal_semWait(){
-  // do stuff :)
+#include "disastrOS_constants.h"
+
+void internal_semWait() {
+  
+  int sem_fd = running->syscall_args[0];
+
+  // Get the SemDescriptor from the opened semaphores
+  SemDescriptor* sem_desc = (SemDescriptor*) SemDescriptorList_byFd(&(running->sem_descriptors), sem_fd );
+  if (!sem_desc) {
+    printf("ERROR: SemDescriptor not found in the process\n");
+    running->syscall_retvalue = DSOS_ERESOURCENOFD;
+    return;
+  }
+
+  // Get the semaphore from its descriptor
+  Semaphore* sem = sem_desc->semaphore;
+
+  // Check the count value. If <= 0 then put the caller on waiting
+  if (sem->count <= 0) {
+
+    SemDescriptorPtr* sem_desc_ptr = sem_desc->ptr;
+    List_insert( &(sem_desc->semaphore->waiting_descriptors),
+                  sem->waiting_descriptors.last,
+                  (ListItem*) sem_desc_ptr ); 
+
+    // Put the caller on waiting
+    running->status = Waiting;
+    List_insert(&waiting_list, waiting_list.last, (ListItem*) running);
+
+    // Start the first process in ready list
+    if(!ready_list.first) {
+        printf("ERROR: No process in the ready list. \nPossilbe deadlock!\n");
+        running->syscall_retvalue = DSOS_EDEADLOCK;
+        return;
+    }
+
+    running = (PCB*) List_detach(&ready_list, ready_list.first);
+  }
+
+  sem->count--;
+  running->syscall_retvalue = 0;
 }
