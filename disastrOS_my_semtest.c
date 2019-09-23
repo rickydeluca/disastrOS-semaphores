b5cf80d96d7e969461c10dc174e3ad7d733fa5dd
Riccardo De Luca <rickydeluca1@gmail.com>
2019-09-23 19:45:33 +0200

    Added test for semWait

--- disastrOS_my_semtest.c
+++ disastrOS_my_semtest.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include    <stdio.h>
+#include    <stdlib.h>
 
-#include "disastrOS.h"
-#include "disastrOS_semaphore.h"
+#include    "disastrOS.h"
+#include    "disastrOS_semaphore.h"
+#include    "disastrOS_semdescriptor.h"
+#include    "disastrOS_constants.h"
+#include    "disastrOS_globals.h"
 
-#define EXIT_FAILURE        1
-#define EXIT_SUCCESSFUL     0
+#define     EXIT_FAILURE        1
+#define     EXIT_SUCCESSFUL     0
 
 /* SEMOPEN TEST */
 void semopen_test(void* args) {
@@ -50,6 +53,33 @@ void semclose_test(void* args) {
     exit(EXIT_SUCCESSFUL);
 }
 
+// SEMWAIT TEST
+void semwait_test(void* args) {
+    int sem_id      = 0;
+    int sem_count   = 30;
+
+    int sem_fd = 0;
+    sem_fd = disastrOS_semOpen(sem_id, sem_count);
+
+    SemDescriptor* sem_desc = (SemDescriptor*) SemDescriptorList_byFd(&(running->sem_descriptors), sem_fd );
+
+    printf("Try testing semWait with 31 iterations");
+
+    int i;
+    for (i = 0; i < 31; i++) {
+        printf("Iteration number: \t %d\n", i);
+        printf("Sem count value: \t %d\n", sem_desc->semaphore->count);
+        if(disastrOS_semWait(sem_fd) != 0) {
+            printf("TEST ERROR: semWait\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("TEST SUCCESS: semWait\n");
+    exit(EXIT_SUCCESS);
+}
+
+
 int main(int argc, char** argv) {
     int test_num;
     
@@ -58,8 +88,8 @@ int main(int argc, char** argv) {
         logfilename = argv[1];
     }
    
-    // TEST: semOpen
-    printf("What would you like to test?\n    1 - semOpen\n    2 - semClose\n");
+    // Single function tests
+    printf("What would you like to test?\n    1 - semOpen\n    2 - semClose\n    3 - semWait\n");
 
     scanf("%d", &test_num);
     if (test_num == 1) {
@@ -68,7 +98,10 @@ int main(int argc, char** argv) {
     } else if (test_num == 2) {
         printf("Testing semClose\n");
         disastrOS_start(semclose_test, 0, logfilename);
-    } else {
+    } else if (test_num == 3) {
+        printf("Testing semWait\n");
+        disastrOS_start(semwait_test, 0, logfilename);
+    }else {
         printf("Wrong input. Sayonara!");
     }
 
